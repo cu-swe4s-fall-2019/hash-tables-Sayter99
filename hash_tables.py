e8850eba694bdc6c46e37e10881acd6ad919238c
Sayter99 <ing5698yi@hotmail.com>
2019-10-12 16:23:00 -0600

    implement hash tables and their tests

--- hash_tables.py
+++ hash_tables.py
@@ -5,25 +5,58 @@ class LinearProbe:
     def __init__(self, N, hash_fucntion):
         self.hash_fucntion = hash_fucntion
         self.N = N
+        self.T = [None for i in range(N)]
+        self.M = 0
 
     def add(self, key, value):
         start_hash = self.hash_fucntion(key, self.N)
-        pass
+        if (start_hash is None):
+            return None
+
+        for i in range(self.N):
+            next_hash = (start_hash + i) % self.N
+            if (self.T[next_hash] is None):
+                self.T[next_hash] = (key, value)
+                self.M += 1
+                return True
+        return False
 
     def search(self, key):
         start_hash = self.hash_fucntion(key, self.N)
-        pass
+        if (start_hash is None):
+            return None
+
+        for i in range(self.N):
+            next_hash = (start_hash + i) % self.N
+            if (self.T[next_hash] is None):
+                return None
+            if (self.T[next_hash][0] == key):
+                return self.T[next_hash][1]
+        return None
 
 
 class ChainedHash:
     def __init__(self, N, hash_fucntion):
         self.hash_fucntion = hash_fucntion
         self.N = N
+        self.T = [[] for i in range(N)]
+        self.M = 0
 
     def add(self, key, value):
         start_hash = self.hash_fucntion(key, self.N)
-        pass
+        if (start_hash is None):
+            return False
+
+        self.T[start_hash].append((key, value))
+        self.M += 1
+        return True
 
     def search(self, key):
         start_hash = self.hash_fucntion(key, self.N)
-        pass
+        if (start_hash is None):
+            return None
+
+        for k, v in self.T[start_hash]:
+            if (key == k):
+                return v
+        return None
--- test_hash.py
+++ test_hash.py
@@ -10,17 +10,54 @@ class TestHash(unittest.TestCase):
         a = hash_functions.h_ascii('ACT', 1000)
         b = hash_functions.h_ascii('CAT', 1000)
         self.assertEqual(a, b)
+        # test invalid N
         a = hash_functions.h_ascii('AAA', 'a')
         self.assertEqual(a, None)
         a = hash_functions.h_ascii('123', 1000)
+        # '1' = 49, '2' = 50, '3' = 51
         self.assertEqual(a, 150)
 
     def test_h_rolling(self):
         a = hash_functions.h_rolling('AB', 1000)
         b = hash_functions.h_rolling('BA', 1000)
         self.assertNotEqual(a, b)
+        # test invalid N
         a = hash_functions.h_ascii('AAA', 'a')
         self.assertEqual(a, None)
+
+    def test_linear_probing(self):
+        t = hash_tables.LinearProbe(1000, hash_functions.h_ascii)
+        t.add('ACT', 10)
+        t.add('123', 20)
+        self.assertEqual(t.search('ACT'), 10)
+        self.assertEqual(t.search('123'), 20)
+        # search the key does not in the table
+        self.assertEqual(t.search('nnn'), None)
+
+        t = hash_tables.LinearProbe(1000, hash_functions.h_rolling)
+        t.add('ACT', 10)
+        t.add('123', 20)
+        self.assertEqual(t.search('ACT'), 10)
+        self.assertEqual(t.search('123'), 20)
+        # search the key does not in the table
+        self.assertEqual(t.search('nnn'), None)
+
+    def test_chained_hash(self):
+        t = hash_tables.ChainedHash(1000, hash_functions.h_ascii)
+        t.add('ACT', 10)
+        t.add('123', 20)
+        self.assertEqual(t.search('ACT'), 10)
+        self.assertEqual(t.search('123'), 20)
+        # search the key does not in the table
+        self.assertEqual(t.search('nnn'), None)
+
+        t = hash_tables.ChainedHash(1000, hash_functions.h_rolling)
+        t.add('ACT', 10)
+        t.add('123', 20)
+        self.assertEqual(t.search('ACT'), 10)
+        self.assertEqual(t.search('123'), 20)
+        # search the key does not in the table
+        self.assertEqual(t.search('nnn'), None)
 
 
 if __name__ == '__main__':
